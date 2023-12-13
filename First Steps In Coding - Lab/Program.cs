cfe3d4c1c1766eb151fdecd6555d2ef4115eedf0
Zlatena Paskaleva <141223652+ZlatenaPaskaleva@users.noreply.github.com>
2023-12-13 19:20:48 +0400

    Add files via upload

--- First Steps In Coding - Lab/Program.cs
+++ First Steps In Coding - Lab/Program.cs
@@ -1,11 +1,5 @@
-﻿Console.WriteLine(1);
-Console.WriteLine(2);
-Console.WriteLine(3);
-Console.WriteLine(4);
-Console.WriteLine(5);
-Console.WriteLine(6);
-Console.WriteLine(7);
-Console.WriteLine(8);
-Console.WriteLine(9);
-Console.WriteLine(10);
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
 
+int area = a * b;
+Console.WriteLine(area);
